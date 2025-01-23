3d5ef43b0be2e949f18d33fbd0375e4e8e393d9b
Ben Landrum <8enlandrum@gmail.com>
2025-01-23 16:32:13 -0500

    way better embeddings to fbin

--- embeddings_to_fbin.py
+++ embeddings_to_fbin.py
@@ -1,30 +1,65 @@
-"""Construct an fbin for gensim embeddings"""
+"""Construct an fbin for gensim embeddings, along with queries, vocab, and ground truth."""
 import gensim.downloader as api
 import numpy as np
+import sys
+from pathlib import Path
+import subprocess
 
-def embeddings_to_fbin(fbin_path=None,
-                       embeddings="glove-wiki-gigaword-50",
-                       normalize=True):
-    if fbin_path is None:
-        fbin_path = f"{embeddings}.fbin"
-    
-    model = api.load(embeddings)
-    n, d = model.vectors.shape
-    
-    
-    if normalize:
-        print("Normalizing vectors...")
-        vectors = model.vectors / np.linalg.norm(model.vectors, axis=1)[:, np.newaxis]
-    else:
-        vectors = model.vectors
-    
-    print(f"Writing {n} vectors of dimension {d} to {fbin_path}")
+
+def numpy_to_fbin(vectors, fbin_path):
+    n, d = vectors.shape
     with open(fbin_path, "wb") as fbin_file:
         np.array([n, d], dtype=np.int32).tofile(fbin_file)
         vectors.astype(np.float32).tofile(fbin_file)
-    print("Done.")
+        
+def words_to_file(words, file_path):
+    with open(file_path, "w") as file:
+        for word in words:
+            file.write(word + "\n")
     
     
 if __name__ == "__main__":
-    embeddings_to_fbin(normalize=False)
+    if len(sys.argv) < 2:
+        print("Usage: python embeddings_to_fbin.py <gensim embedding name>")
+        sys.exit(1)
+        
+    embeddings = sys.argv[1]
+    
+    if Path(f"data/{embeddings}").exists():
+        print("Embeddings already downloaded, but running again.")
+    
+    download_dir = Path(f"data/{embeddings}")
+    download_dir.mkdir(parents=True, exist_ok=True)
+    
+    print(f"Downloading {embeddings}...")
+    model = api.load(embeddings)
+    vectors = model.vectors
+    vocab = model.index_to_key
+    
+    print("Saving embeddings...")
+    numpy_to_fbin(vectors, download_dir / "base.fbin")
+    words_to_file(vocab, download_dir / "vocab.txt")
+    
+    nq = 10000
+    print(f"Saving {nq} queries...")
+    query_indices = np.random.choice(min(len(vocab) // 50, 30000), nq, replace=False)
+    query_words = [vocab[i] for i in query_indices]
+    query_vectors = vectors[query_indices]
+    
+    numpy_to_fbin(query_vectors, download_dir / "query.fbin")
+    words_to_file(query_words, download_dir / "query.txt")
+    
+    print("Computing (and saving) ground truth...")
+    
+    if not Path("ParlayANN/data_tools/compute_groundtruth").exists():
+        print("Please compile ParlayANN/data_tools/compute_groundtruth (run make in ParlayANN/data_tools/)")
+        sys.exit()
+    
+    subprocess.run(["ParlayANN/data_tools/compute_groundtruth",
+                    "-base_path", download_dir / "base.fbin",
+                    "-query_path", download_dir / "query.fbin",
+                    "-gt_path", download_dir / "GT",
+                    "-k", "1000",
+                    "-dist_func", "Euclidian",
+                    "-data_type", "float"])
     
