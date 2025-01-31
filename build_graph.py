4cfbe684a333eed74e0ea11534dc0c1bfe90b51b
Ben Landrum <8enlandrum@gmail.com>
2025-01-31 13:08:02 -0500

    added logging and parameterization

--- build_graph.py
+++ build_graph.py
@@ -1,13 +1,21 @@
 from ParlayANN.python import wrapper as wp
 
 import sys
-import os
 from pathlib import Path
 import numpy as np
 
 from utils import graph_file_to_list_of_lists, read_vocab, fbin_to_numpy
+from beam_search import eager_beam_search, beam_search
+from tqdm import tqdm
 
 GRAPH_TYPES = ["pynndescent", "vamana", "hcnng"]
+
+VAMANA_PARAMS = {
+    "R": 8,
+    "L": 75,
+    "alpha": .99,
+    "two_pass": True
+}
 
 if len(sys.argv) < 2:
     print("Usage: python build_graph.py <embedding name> <graph type>")
@@ -34,7 +42,7 @@ if '--dont-build' not in sys.argv:
     if graph_type == "pynndescent":
         wp.build_pynndescent_index("Euclidian", "float", data_dir / "base.fbin", data_dir / "outputs" / "pynndescent", 40, 10, 100, 1.2, .05)
     elif graph_type == "vamana":
-        wp.build_vamana_index("mips", "float", data_dir / "base.fbin", data_dir / "outputs" / "vamana", 8, 100, 0.99, False)
+        wp.build_vamana_index("mips", "float", data_dir / "base.fbin", data_dir / "outputs" / "vamana", **VAMANA_PARAMS)
     elif graph_type == "hcnng":
         wp.build_hcnng_index("Euclidian", "float", data_dir / "base.fbin", data_dir / "outputs" / "hcnng", 40, 20, 1000)
     
@@ -58,8 +66,8 @@ percentiles = [0, 25, 50, 75, 90, 95, 99, 100]
 for p in percentiles:
     print(f"{p}%: {np.percentile([len(neighbors) for neighbors in graph], p)}")
     
-vocab = read_vocab(data_dir / "vocab.txt")
-query_vocab = read_vocab(data_dir / "query.txt")
+_, vocab = read_vocab(data_dir / "vocab.txt")
+_, query_vocab = read_vocab(data_dir / "query.txt")
 
 query_indices = [vocab.index(word) for word in query_vocab]
 
@@ -68,10 +76,35 @@ vectors = fbin_to_numpy(data_dir / "base.fbin")
 visited_counts = []
 compared_counts = []
 
-for idx in query_indices:
-    visited, compared = wp.eager_beam_search(graph, vectors, 0, idx)
+# print("eager beam search:")
+# for idx in tqdm(query_indices):
+#     visited, compared = eager_beam_search(graph, vectors, 0, idx)
+#     visited_counts.append(len(visited))
+#     compared_counts.append(len(compared))
+
+# print(f"average visited: {np.mean(visited_counts)}")
+# print(f"average compared: {np.mean(compared_counts)}")
+
+# visited_counts = []
+# compared_counts = []
+
+print("beam search:")
+for idx in tqdm(query_indices[:1000]):
+    visited, compared = eager_beam_search(graph, vectors, 0, idx)
     visited_counts.append(len(visited))
     compared_counts.append(len(compared))
 
 print(f"average visited: {np.mean(visited_counts)}")
 print(f"average compared: {np.mean(compared_counts)}")
+
+converged = np.array(visited_counts) < 1000
+print(f"recall: {np.mean(converged)}")
+
+log_file = data_dir / "outputs" / f"{graph_type}_log.csv"
+
+if not log_file.exists():
+    with open(log_file, "w") as f:
+        f.write(f"visited,compared,{",".join(VAMANA_PARAMS.keys())}\n")
+        
+with open(log_file, "a") as f:
+    f.write(f"{np.mean(visited_counts)},{np.mean(compared_counts)},{','.join(str(value) for value in VAMANA_PARAMS.values())}\n")
